c6d9c0d0cf8201135f1e9204e6a571ebc1a8d409
Samuel Jim <jimsamuel50@gmail.com>
2021-02-20 15:31:44 +0100

    Added mysql commands

--- main.go
+++ main.go
@@ -1,27 +1,64 @@
-package main 
+package main
 
 import (
-	"fmt"
 	"database/sql"
-	_"github.com/go-sql-driver/mysql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func main(){
-	fmt.Println("connection to the Database")
+type Post struct {
+	ID          int
+	Name        string
+	Description string
+}
 
-	db, err := sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/test")
-
-	if err != nil{
-		panic (err.Error())
-	}
+func main() {
+	db, err := sql.Open("mysql", "root:root@/gotest")
+	errorCheck(err)
 
 	defer db.Close()
+	pingDB(db)
 
-	insert, err := db.Query("INSERT INTO test VALUES(2, 'Test')")
+	// insert into DB
 
-	if err != nil {
-		panic (err.Error())
+	stmt, err := db.Prepare("INSERT INTO posts(id, Name) VALUES(?,?)")
+	errorCheck(err)
+	result, err := stmt.Exec(5, "cool course")
+	errorCheck(err)
+	id, err := result.LastInsertId()
+	errorCheck(err)
+	fmt.Println("insert Data", id)
+
+	// update DB
+
+	stmt2, err := db.Prepare("UPDATE posts SET Name=? WHERE id=?")
+	errorCheck(err)
+	result2, err := stmt2.Exec("Goddamn", 5)
+	errorCheck(err)
+	id2, err := result2.RowsAffected()
+	errorCheck(err)
+	fmt.Println(id2)
+
+	// return values
+
+	stmt3, err := db.Query("SELECT * FROM posts")
+	errorCheck(err)
+	var post = Post{}
+	for stmt3.Next() {
+		err := stmt3.Scan(&post.ID, &post.Description, &post.Name)
+		errorCheck(err)
+		fmt.Println(post)
 	}
+}
 
-	defer insert.Close()
+func errorCheck(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func pingDB(db *sql.DB) {
+	err := db.Ping()
+	errorCheck(err)
 }
