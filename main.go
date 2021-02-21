a0628932bf2e69f03125b436520c4fdcc0611588
Samuel Jim <jimsamuel50@gmail.com>
2021-02-21 13:39:39 +0100

    Modified template files and main file

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// Post struct
 type Post struct {
 	ID          int
 	Name        string
@@ -27,7 +28,7 @@ func pingDB(db *sql.DB) {
 }
 
 func dbConn() (db *sql.DB) {
-	db, err := sql.Open("mysql", "root:root@/gotest")
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:8889)/gotest")
 	errorCheck(err)
 	return db
 }
@@ -36,22 +37,22 @@ var tmpl = template.Must(template.ParseGlob("forms/*"))
 
 func index(w http.ResponseWriter, r *http.Request) {
 	db := dbConn()
-	results, err := db.Query("SELECT * FROM posts")
+	results, err := db.Query("SELECT * FROM post")
 	errorCheck(err)
 
 	pst := Post{}
 	res := []Post{}
 
 	for results.Next() {
-		var ID int
-		var Name string
-		var Description string
+		var id int
+		var name string
+		var description string
 
-		err := results.Scan(&ID, &Name, &Description)
+		err = results.Scan(&id, &name, &description)
 		errorCheck(err)
-		pst.ID = ID
-		pst.Name = Name
-		pst.Description = Description
+		pst.ID = id
+		pst.Name = name
+		pst.Description = description
 
 		res = append(res, pst)
 
@@ -62,28 +63,25 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func show(w http.ResponseWriter, r *http.Request) {
 	db := dbConn()
-
-	postID := r.URL.Query().Get("ID")
-
-	result, err := db.Query("SELECT * FROM posts WHERE id=?", postID)
-	errorCheck(err)
-
-	pst := Post{}
-	for result.Next() {
-		var ID int
-		var Name string
-		var Description string
-
-		err := result.Scan(&ID, &Name, &Description)
-		errorCheck(err)
-
-		pst.ID = ID
-		pst.Name = Name
-		pst.Description = Description
-
-		tmpl.ExecuteTemplate(w, "Show", pst)
-		defer db.Close()
+	nID := r.URL.Query().Get("id")
+	selDB, err := db.Query("SELECT * FROM Post WHERE id=?", nID)
+	if err != nil {
+		panic(err.Error())
 	}
+	emp := Post{}
+	for selDB.Next() {
+		var id int
+		var name, description string
+		err = selDB.Scan(&id, &name, &description)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp.ID = id
+		emp.Name = name
+		emp.Description = description
+	}
+	tmpl.ExecuteTemplate(w, "Show", emp)
+	defer db.Close()
 }
 
 func new(w http.ResponseWriter, r *http.Request) {
@@ -93,8 +91,8 @@ func new(w http.ResponseWriter, r *http.Request) {
 func edit(w http.ResponseWriter, r *http.Request) {
 	db := dbConn()
 
-	postID := r.URL.Query().Get("ID")
-	result, err := db.Query("SELECT * FROM posts WHERE id=?", postID)
+	postID := r.URL.Query().Get("id")
+	result, err := db.Query("SELECT * FROM post WHERE id=?", postID)
 	errorCheck(err)
 
 	pst := Post{}
@@ -119,7 +117,7 @@ func delete(w http.ResponseWriter, r *http.Request) {
 	db := dbConn()
 
 	postID := r.URL.Query().Get("ID")
-	stmt, err := db.Prepare("DELETE * FROM posts WHERE id=?")
+	stmt, err := db.Prepare("DELETE FROM post WHERE id=?")
 	errorCheck(err)
 
 	stmt.Exec(postID)
@@ -127,7 +125,6 @@ func delete(w http.ResponseWriter, r *http.Request) {
 	log.Println("Resource deleted")
 	defer db.Close()
 	http.Redirect(w, r, "/", 301)
-
 }
 
 func insert(w http.ResponseWriter, r *http.Request) {
@@ -137,7 +134,7 @@ func insert(w http.ResponseWriter, r *http.Request) {
 		name := r.FormValue("name")
 		description := r.FormValue("description")
 
-		insform, err := db.Prepare("INSERT INTO posts (name, description) VALUES(?,?)")
+		insform, err := db.Prepare("INSERT INTO post (name, description) VALUES(?,?)")
 		errorCheck(err)
 
 		insform.Exec(name, description)
@@ -156,7 +153,7 @@ func update(w http.ResponseWriter, r *http.Request) {
 		description := r.FormValue("description")
 		id := r.FormValue("uid")
 
-		stmt, err := db.Prepare("UPDATE posts SET name=?, description=? WHERE id=?")
+		stmt, err := db.Prepare("UPDATE post SET name=?, description=? WHERE id=?")
 		errorCheck(err)
 
 		stmt.Exec(name, description, id)
@@ -167,7 +164,7 @@ func update(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	log.Println("server started")
+	log.Println("server started at ")
 	http.HandleFunc("/", index)
 	http.HandleFunc("/show", show)
 	http.HandleFunc("/new", new)
