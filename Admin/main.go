deeb851aced2433ecc68f838159954e7089e964e
Samuel Jim <jimsamuel50@gmail.com>
2021-03-18 08:30:22 +0100

    working on admin end

--- Admin/main.go
+++ Admin/main.go
@@ -1,46 +1,25 @@
-package main
+package main 
 
-import (
-	"database/sql"
-
-	_ "github.com/go-sql-driver"
-)
-
-// Post struct
-type Post struct {
-	ID          int
-	Name        string
-	Description string
-	CreatedAt   string
+// Admin structure
+type Admin struct{
+	ID float64
+	Name string 
+	Designation string
 }
 
-func panicErrorCheck(err error) {
+func errorCheck(err error){
 	if err != nil {
 		panic(err.Error())
 	}
 }
 
-func databasePing(db *sql.DB) {
-	err := db.Ping()
-	panicErrorCheck(err)
+func dbPing(db *sql.DB){
+	db, err := db.Ping()
+	errorCheck(err)
 }
 
-func dbConn() (db *sql.DB) {
+func dbConnection()(db *sql.DB){
 	db, err := sql.Open("mysql", "root:root@tcp(localhost:8889)/gotest")
-	panicErrorCheck(err)
+	errorCheck(err)
 	return db
-}
-
-func checkTotalNumberOfPosts() {
-	db := dbConn()
-
-	results, err := db.Query("SELECT COUNT(id) AS countPosts FROM posts")
-	panicErrorCheck(err)
-
-	var countPosts int64
-	for results.Next() {
-		err := results.Scan(&countPosts)
-		panicErrorCheck(err)
-	}
-	return countPosts, nil
 }
